cb27c992a619128bb4ad0600fb7d549ea9db58fe
Tim Cope <kirbycope@gmail.com>
2022-01-11 14:54:49 -0800

    Cleanup

--- reset.ps1
+++ reset.ps1
@@ -1,23 +1,23 @@
 ﻿# Undo Level Edits and Play Data
 $gameDirectory = "$env:APPDATA\.minecraft";
 cd $gameDirectory\saves\CopeCraft
-rm $gameDirectory\saves\CopeCraft\advancements\76ca1596-ad31-456d-93eb-55e42ab91255.json
+rm $gameDirectory\saves\CopeCraft\advancements\76ca1596-ad31-456d-93eb-55e42ab91255.json -ErrorAction SilentlyContinue
 git restore $gameDirectory\saves\CopeCraft\data\raids.dat
-rm $gameDirectory\saves\CopeCraft\data\scoreboard.dat
+rm $gameDirectory\saves\CopeCraft\data\scoreboard.dat -ErrorAction SilentlyContinue
 git restore $gameDirectory\saves\CopeCraft\DIM-1\data\raids.dat
 git restore $gameDirectory\saves\CopeCraft\DIM1\data\raids_end.dat
 git restore $gameDirectory\saves\CopeCraft\entities\r.-1.-1.mca
-git restore $gameDirectory\saves\CopeCraft\entities\r.-1.0.mca -ErrorAction SilentlyContinue
+git restore $gameDirectory\saves\CopeCraft\entities\r.-1.0.mca
 git restore $gameDirectory\saves\CopeCraft\entities\r.0.0.mca
 git restore $gameDirectory\saves\CopeCraft\level.dat
-rm $gameDirectory\saves\CopeCraft\level.dat_old
-rm $gameDirectory\saves\CopeCraft\playerdata\76ca1596-ad31-456d-93eb-55e42ab91255.dat
-rm $gameDirectory\saves\CopeCraft\playerdata\76ca1596-ad31-456d-93eb-55e42ab91255.dat_old
+rm $gameDirectory\saves\CopeCraft\level.dat_old -ErrorAction SilentlyContinue
+rm $gameDirectory\saves\CopeCraft\playerdata\76ca1596-ad31-456d-93eb-55e42ab91255.dat -ErrorAction SilentlyContinue
+rm $gameDirectory\saves\CopeCraft\playerdata\76ca1596-ad31-456d-93eb-55e42ab91255.dat_old -ErrorAction SilentlyContinue
 git restore $gameDirectory\saves\CopeCraft\region\r.-1.-1.mca
 git restore $gameDirectory\saves\CopeCraft\region\r.-1.0.mca
 git restore $gameDirectory\saves\CopeCraft\region\r.0.-1.mca
 git restore $gameDirectory\saves\CopeCraft\region\r.0.0.mca
-rm $gameDirectory\saves\CopeCraft\session.lock
-rm $gameDirectory\saves\CopeCraft\stats\76ca1596-ad31-456d-93eb-55
-rm $gameDirectory\saves\CopeCraft\resources.zip
+rm $gameDirectory\saves\CopeCraft\session.lock -ErrorAction SilentlyContinue
+rm $gameDirectory\saves\CopeCraft\stats\76ca1596-ad31-456d-93eb-55 -ErrorAction SilentlyContinue
+rm $gameDirectory\saves\CopeCraft\resources.zip -ErrorAction SilentlyContinue
 Compress-Archive -Path $gameDirectory\saves\CopeCraft\_resourcepack -DestinationPath  $gameDirectory\saves\CopeCraft\resources.zip
